1643fe68cabc9ba70a18945017ff049e99996e1b
Urbanovich Dzmitry <urbanovich.mslo@gmail.com>
2020-08-24 23:27:55 +0300

    updated readme

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'SoftBuild_HitPay',
+    'SoftBuild___HitPay',
     __DIR__
 );
